331e20f5271a04363a77ed156fdf3149ea2d856f
Gaetan Antonio <rantoniogaetan@gmail.com>
2021-04-08 10:54:48 +0300

    Premire version de dossier et fichier de conf

--- src/com/main/apps/A3_suivi_saisie.java
+++ src/com/main/apps/A3_suivi_saisie.java
@@ -18,6 +18,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -55,7 +57,7 @@ public class A3_suivi_saisie {
         String password  = "";
         
 	// VERIFIER SI FICHIER DE CONF EXISTE
-        String racine = new File("").getAbsolutePath();
+        //String racine = new File("").getAbsolutePath();
         String path = new File("").getAbsolutePath()+"\\conf";
         String filePathAndName = path + "\\config.json";
 
@@ -66,15 +68,90 @@ public class A3_suivi_saisie {
             
             System.out.println("Dossier de configuration introuvable !");
             JOptionPane jop = new JOptionPane();
-            int option = jop.showConfirmDialog(null, "Dossier de configuration introuvable !\nVoulez-vous lancer le procédure de création du dossier de configuration ?","" , JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            int option = jop.showConfirmDialog(null, "Dossier de configuration introuvable !\nVoulez-vous lancer le procédure de création du dossier et du fichier de configuration ?","" , JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
             if(option == JOptionPane.OK_OPTION){
                 
+                // creation du dossier de configuration
                 System.out.println("Creation dossier ...");
                 folder.mkdir();
+                
+                    try{
+                        
+                        
+                        System.out.println("Creation fichier conf ...");
+                        file.createNewFile();
+                        
+                        JTextField HoteName = new JTextField();
+                        JTextField nameBdd = new JTextField();
+                        JTextField portBdd = new JTextField();
+                        JTextField userName = new JTextField();
+                        JPasswordField passe = new JPasswordField();
+                        
+                                                
+                        int optionConfigs = JOptionPane.showOptionDialog(null, 
+                          new Object[] {"Nom d'hôte :", HoteName, "Nom de la base de données :", nameBdd, "Port :", portBdd, "Nom d'utilisateur :", userName, "Mot de passe :", passe},
+                          "Connexion",
+                          JOptionPane.OK_CANCEL_OPTION,
+                          JOptionPane.QUESTION_MESSAGE, null, null, null);
+                        
+                        if(optionConfigs == 0 ){
+
+                        }else{
+                            
+                            System.out.println("Vous les vous vraimenet quitter le processus de création du dossier et fichier de configuration ?");
+                            JOptionPane jopCreateFolderAndFile = new JOptionPane();
+                            int option2 = jopCreateFolderAndFile.showConfirmDialog(null, "Annulation processus de création (dossier et fichier de conf) !\nVous les vous vraimenet quitter le processus de création du dossier et fichier de configuration ?","" , JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                            
+                            if(option2 == JOptionPane.OK_OPTION){
+                                System.exit(0);
+                            }else{
+                                int optionConfigs2 = JOptionPane.showOptionDialog(null, 
+                                new Object[] {"Nom d'hôte :", HoteName, "Nom de la base de données :", nameBdd, "Port :", portBdd, "Nom d'utilisateur :", userName, "Mot de passe :", passe},
+                                "Connexion",
+                                JOptionPane.OK_CANCEL_OPTION,
+                                JOptionPane.QUESTION_MESSAGE, null, null, null);
+                            }
+                        }
+                        
+                       
+                            System.out.println("Nom d'hôte :" + HoteName.getText()+ "Nom de la base de données :"+ nameBdd.getText()+ "Nom d'utilisateur :"+ userName.getText()+ "Mot de passe :"+ passe.getText());
+                       
+                        
+                        
+                        // remplissage de l'objet JSON
+                        JSONObject j = new JSONObject();
+                        j.put("host",  HoteName.getText());
+                        j.put("port", portBdd.getText());
+                        j.put("dbname", nameBdd.getText());
+                        j.put("user", userName.getText());
+                        j.put("password", passe.getText());
+                        
+  
+                        try(FileWriter fileJson = new FileWriter(filePathAndName)){
+                            
+                            System.out.println("Remplissage du fichier de conf ...");
+                            
+                            fileJson.write(j.toString());
+                            fileJson.flush();
+                            
+                            
+                            System.out.println("dossier et fichier conf trouvé ! .......");
+
+                        }catch(IOException e){
+                            e.printStackTrace();
+                        }
+
+                    }catch(IOException f){
+                        f.printStackTrace();
+                    } 
             }
    
-        }else if(!file.exists()){
+        }else{
+            // dossier existe déjà mais le fichier de conf pas encore
             
+            
+            if(!file.exists()){
+                
                 System.out.println("Fichier de configuration introuvable !");
                 
                 JOptionPane jop2 = new JOptionPane();
@@ -88,14 +165,50 @@ public class A3_suivi_saisie {
                         System.out.println("Creation fichier conf ...");
                         file.createNewFile();
                         
+                        JTextField HoteName = new JTextField();
+                        JTextField nameBdd = new JTextField();
+                        JTextField portBdd = new JTextField();
+                        JTextField userName = new JTextField();
+                        JPasswordField passe = new JPasswordField();
                         
-                        // remplissage du fichier
+                                                
+                        int optionConfigs = JOptionPane.showOptionDialog(null, 
+                          new Object[] {"Nom d'hôte :", HoteName, "Nom de la base de données :", nameBdd, "Port :", portBdd, "Nom d'utilisateur :", userName, "Mot de passe :", passe},
+                          "Connexion",
+                          JOptionPane.OK_CANCEL_OPTION,
+                          JOptionPane.QUESTION_MESSAGE, null, null, null);
+                        
+                        if(optionConfigs == 0 ){
+
+                        }else{
+                            
+                            System.out.println("Vous les vous vraimenet quitter le processus de création du dossier et fichier de configuration ?");
+                            JOptionPane jopCreateFolderAndFile = new JOptionPane();
+                            int optionFileCreate = jopCreateFolderAndFile.showConfirmDialog(null, "Annulation processus de création (dossier et fichier de conf) !\nVous les vous vraimenet quitter le processus de création du dossier et fichier de configuration ?","" , JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                            
+                            if(optionFileCreate == JOptionPane.OK_OPTION){
+                                System.exit(0);
+                            }else{
+                                int optionConfigs2 = JOptionPane.showOptionDialog(null, 
+                                new Object[] {"Nom d'hôte :", HoteName, "Nom de la base de données :", nameBdd, "Port :", portBdd, "Nom d'utilisateur :", userName, "Mot de passe :", passe},
+                                "Connexion",
+                                JOptionPane.OK_CANCEL_OPTION,
+                                JOptionPane.QUESTION_MESSAGE, null, null, null);
+                            }
+                        }
+                        
+                       
+                            System.out.println("Nom d'hôte :" + HoteName.getText()+ "Nom de la base de données :"+ nameBdd.getText()+ "Nom d'utilisateur :"+ userName.getText()+ "Mot de passe :"+ passe.getText());
+                       
+                        
+                        
+                        // remplissage de l'objet JSON
                         JSONObject j = new JSONObject();
-                        j.put("host", "127.0.0.1");
-                        j.put("port", "5432");
-                        j.put("dbname", "oprod");
-                        j.put("user", "postgres");
-                        j.put("password", "2021.");
+                        j.put("host",  HoteName.getText());
+                        j.put("port", portBdd.getText());
+                        j.put("dbname", nameBdd.getText());
+                        j.put("user", userName.getText());
+                        j.put("password", passe.getText());
                         
   
                         try(FileWriter fileJson = new FileWriter(filePathAndName)){
@@ -116,56 +229,15 @@ public class A3_suivi_saisie {
                         f.printStackTrace();
                     }
                 }
-            }else{
-            
-            //connectDatabase = new ConnectDb("127.0.0.15", 5432, "oprod", "2021.", "postgres").getConnection();
-            
-            
-        // IL FAUT LIRE LE FICHIER DE CONFIGURATION
-        
-        JSONParser parser = new JSONParser();
-        
-        try{
-            Object obj = parser.parse(new FileReader(filePathAndName));
-            
-            JSONObject jsonObject = (JSONObject) obj;
+            }   // FIN !file.exists()
+        } // FIN ELSE !folder.exists()
 
-            
-            host = (String) jsonObject.get("host");
-            port = (String) jsonObject.get("port");
-            dbname = (String) jsonObject.get("dbname");
-            user = (String) jsonObject.get("user");
-            password = (String) jsonObject.get("password");
-            
-
-            //System.out.println("Port dans le fichier : " + dbname);
-            
-            
-        }
-        catch(FileNotFoundException e) {e.printStackTrace();}
-        catch(IOException e) {e.printStackTrace();}
-        catch(ParseException e) {e.printStackTrace();}
-        catch(Exception e) {e.printStackTrace();}
-        
-        
-            
-        //String host = "127.0.0.1";
-        //String port = "5432";
-        //String dbname = "oprod";
-        //String user = "postgres";
-        //String password = "2021.";
-        
-
-
-        //Connection connexion = null;
-        
         try{
             
             Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://"+host+":"+port+"/"+dbname;
+            String url = "jdbc:postgresql://"+"127.0.0.1"+":"+"5432"+"/"+"oprod";
 
-            Connection connexion  = DriverManager.getConnection(url, user, password);
-            
+            Connection connexion  = DriverManager.getConnection(url, "postgres", "2021.");
             
             //System.out.println("Lancement de l'application...  : ");
             UserFormDialog home = new UserFormDialog();
@@ -179,10 +251,8 @@ public class A3_suivi_saisie {
             JOptionPane.showMessageDialog(null, "Vérifier votre fichier de configuration\n\n" + e.getMessage(), "Connexion dans la base de données impossible", JOptionPane.INFORMATION_MESSAGE);
             
         }
-             
-            
-        }
         
-    }
+        
+    }  // FIN CLASS MAIN
     
 }
